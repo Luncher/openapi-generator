a895857808acb7472080b325d7567366bed61328
linchen <linchen@boqii.com>
2017-05-17 21:04:42 +0800

    refactor($poplar): update poplar adapter

--- src/adapter/poplar_adapter.js
+++ src/adapter/poplar_adapter.js
@@ -1,19 +1,83 @@
+const DEFAULT_CONSUMES = [
+  'application/json',
+  'application/x-www-form-urlencoded'
+]
+
+const DEFAULT_PRODUCES = [
+  'application/json'  
+]
+
+const DEFAULT_RESPONSE = {
+  "405": {
+    "description": "Invalid input"
+  }
+}
+
 function PoplarSwaggerAdapter (APIRouter) {
   this.APIRouter = APIRouter
 }
 
-PoplarSwaggerAdapter.prototype.parse = function () {
+PoplarSwaggerAdapter.prototype.parseEntity = function (name) {
+
+}
+
+PoplarSwaggerAdapter.prototype.parseMethod = function (path, method, options) {
+  const { createDefinition } = options
+  const config = {
+    tags: [],
+    summary: method.name,
+    description: method.name,
+    verb: method.http.verb,
+    description: method.description,
+    operationId: method.http.path
+  }
+
+  const paramsIn = config.verb === 'GET' ? 'query' : 'body';      
+  config.consumes = DEFAULT_CONSUMES
+  config.produces = DEFAULT_PRODUCES
+  config.parameters = method.accepts.map(paramter => {
+    if (paramter.entity) {
+      createDefinition(this.parseEntity(paramter.entity))
+    }
+    return {
+      in: paramsIn,
+      name: paramter.arg,
+      type: paramter.type,
+      description: paramter.description || "",
+      schema: paramter.entity ? {"$ref": "#/definitions/" + paramter.entity} : {},
+      required: paramter.required || (paramter.validates && paramter.validates.required)
+    }
+  })
+
+  return createAction(path, config)  
+}
+
+PoplarSwaggerAdapter.prototype.parse = function (options) {
   const APIRouter = this.APIRouter
+  const { 
+    createTag,
+    createPath,
+    createAction,
+    createSecrity,
+    createDefinition,
+    setProperty,
+    callOnDone
+  } = options
+
   Object.keys(APIRouter._apiBuilders).forEach(name => {
     debug('apiBuilder name: ', name)
     debug('apiBuilder methods:')
+
+    const path = createPath({ name })
     const apiBuilder = APIRouter._apiBuilders[name]
+
     Object.keys(apiBuilder._methods).forEach(name => {
+      const method = apiBuilder._methods[name]
+      const action = this.parseMethod(path, [tag], method, options)
       debug('apiMethod name: ', name)
-      const apiMethod = apiBuilder._methods[name]
-      debug('apiMethod description: ', apiMethod.description)    
-      debug('apiMethod accepts: ', apiMethod.accepts)
-      debug('apiMethod http: ', apiMethod.http)
+      debug('apiMethod description: ', method.description)    
+      debug('apiMethod accepts: ', method.accepts)
+      debug('apiMethod http: ', method.http)
     })
   })
 }
