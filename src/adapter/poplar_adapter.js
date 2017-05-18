5e48a0e738545001bf807968a3965a7691178470
linchen <linchen@boqii.com>
2017-05-18 14:15:04 +0800

    refactor($adapter): update poplar adapter
    update poplar adapter parse schema and entity

--- src/adapter/poplar_adapter.js
+++ src/adapter/poplar_adapter.js
@@ -1,44 +1,86 @@
-const DEFAULT_CONSUMES = [
-  'application/json',
-  'application/x-www-form-urlencoded'
-]
-
-const DEFAULT_PRODUCES = [
-  'application/json'  
-]
-
-const DEFAULT_RESPONSE = {
-  "405": {
-    "description": "Invalid input"
-  }
-}
+const assert = require('assert')
+const config = require('../config');
+const Entity = require('poplar').Entity
 
 function PoplarSwaggerAdapter (APIRouter) {
   this.APIRouter = APIRouter
 }
 
-PoplarSwaggerAdapter.prototype.parseEntity = function (name) {
+PoplarSwaggerAdapter.prototype.parseResponse = function (method, options) {
+  const responses = Object.assign({}, config.DEFAULT_RESPONSE)
+  const entity = method.presenter || (method.notes && method.notes.entity)
+  const schema = method.notes && method.notes.schema
+  // assert(Entity.isEntity(entity), 'presenter or notes.entity must be a valid Entity');
+  const excepts = entity._excepts.slice()
+  const exposes = entity._mappings
+  const definition = {
+    type: 'object',
+    name: entity._name,
+    properties: {}
+  }
 
+  Object.keys(schema)
+    .concat(Object.keys(exposes))
+    .forEach(k => {
+      const conf = exposes[k] || schema[k]
+      const type = conf.type.toLowerCase()
+      const def = Object.assign({}, config.MONGOOSE_TYPE_MAPPING[type])
+      if (conf.enum) {
+        def.enum = conf.enum
+      }
+      if (conf.default) {
+        def.default = conf.default
+      }
+      definition.properties = Object.assign(definition.properties, def)
+    })
+  
+  //remove alias key
+  Object.keys(exposes).forEach(k => {
+    if (exposes[k].act === 'alias') {
+      definition.properties[exposes[k].name] = definition.properties[k]
+      delete definition.properties[k]      
+    }
+  })
+
+  //remove excepts field
+  exports.forEach(k => {
+    delete definition.properties[k]    
+  })
+
+  const resp = {
+    "200": {
+      "description": "successful operation",
+      "schema": {
+        "$ref": "#/definitions/" + definition.name
+      }
+    }
+  }
+
+  //TODO parse nested array 、 object、entity using
+  options.createDefinition(definition)
+
+  return Object.assign(responses, resp)
 }
 
-PoplarSwaggerAdapter.prototype.parseMethod = function (path, method, options) {
-  const { createDefinition } = options
+/**
+ * 把response 的entity信息添加到method.notes里面
+ */
+PoplarSwaggerAdapter.prototype.parseMethod = function (tags, method, options) {
   const config = {
-    tags: [],
+    tags,
     summary: method.name,
     description: method.name,
     verb: method.http.verb,
-    description: method.description,
     operationId: method.http.path
   }
+  const { createAction, createPath } = options  
+  const pathName = '/' + method._apiBuilder.name + '/' + config.operationId
+  const path = createPath({ name: pathName })
 
   const paramsIn = config.verb === 'GET' ? 'query' : 'body';      
-  config.consumes = DEFAULT_CONSUMES
-  config.produces = DEFAULT_PRODUCES
+  config.consumes = config.DEFAULT_CONSUMES
+  config.produces = config.DEFAULT_PRODUCES
   config.parameters = method.accepts.map(paramter => {
-    if (paramter.entity) {
-      createDefinition(this.parseEntity(paramter.entity))
-    }
     return {
       in: paramsIn,
       name: paramter.arg,
@@ -49,37 +91,33 @@ PoplarSwaggerAdapter.prototype.parseMethod = function (path, method, options) {
     }
   })
 
+  config.responses = this.parseResponse(method, options)
+
   return createAction(path, config)  
 }
 
 PoplarSwaggerAdapter.prototype.parse = function (options) {
   const APIRouter = this.APIRouter
-  const { 
-    createTag,
-    createPath,
-    createAction,
-    createSecrity,
-    createDefinition,
-    setProperty,
-    callOnDone
-  } = options
+  const { createTag, callOnDone } = options
 
-  Object.keys(APIRouter._apiBuilders).forEach(name => {
-    debug('apiBuilder name: ', name)
+  Object.keys(APIRouter._apiBuilders).forEach(tagName => {
+    debug('apiBuilder name: ', tagName)
     debug('apiBuilder methods:')
 
-    const path = createPath({ name })
-    const apiBuilder = APIRouter._apiBuilders[name]
+    const apiBuilder = APIRouter._apiBuilders[tagName]
+    const tag = createTag({ name: tagName, description: `The ${name} tag`})
 
     Object.keys(apiBuilder._methods).forEach(name => {
       const method = apiBuilder._methods[name]
-      const action = this.parseMethod(path, [tag], method, options)
+      const action = this.parseMethod([tagName], method, options)
       debug('apiMethod name: ', name)
       debug('apiMethod description: ', method.description)    
       debug('apiMethod accepts: ', method.accepts)
       debug('apiMethod http: ', method.http)
     })
   })
+
+  process.nextTick(callOnDone)
 }
 
 PoplarSwaggerAdapter.prototype.getBasePath = function () {
---
+++ src/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  SWAGGER_VERSION: "2.0",
+  DEFAULT_CONSUMES: [
+    'application/json',
+    'application/x-www-form-urlencoded'
+  ],
+  DEFAULT_PRODUCES: [
+    'application/json'  
+  ],
+  MONGOOSE_TYPE_MAPPING: {
+    'string': { type: 'string' },
+    'objectId': { type: 'string' },
+    'number': { type: 'number',  format: 'float' },
+    'date': { type: 'string', format: 'date-time' }
+  },
+  DEFAULT_RESPONSE: {
+    "405": {
+      "description": "Invalid input"
+    },
+    "200": {
+      "description": "successful operation"
+    }
+  }
+}
