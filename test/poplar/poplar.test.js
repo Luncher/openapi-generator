55aa2a3edd0e7d41624f12b4f7a0d0697126ef2d
linchen <linchen@boqii.com>
2017-05-18 14:15:45 +0800

    test($poplar): update poplar test

--- test/poplar/poplar.test.js
+++ test/poplar/poplar.test.js
@@ -1,1 +1,18 @@
 const APIRouter = require('./api/api_routes').v1
+const SWaggerGenerator = require('../../src/')
+const PoplarAdapter = require('../../src/adapter/poplar_adapter')
+
+const options = {
+  type: 'json',
+  version: 'v2',  
+  host: '127.0.0.1',
+  title: 'boqii test title',
+  description: 'The test poplar',
+}
+
+const adapter = new PoplarAdapter({ APIRouter })
+const generator = new SWaggerGenerator(options)
+
+generator.parse(adapter, function (err, spec) {
+
+})
