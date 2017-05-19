c981df4b5f4ceb98db6c1b94971bfac0cc68b0f6
linchen <linchen@boqii.com>
2017-05-19 15:26:46 +0800

    refactor($adapter): update poplar adapter

--- src/adapter/poplar_adapter.js
+++ src/adapter/poplar_adapter.js
@@ -3,57 +3,125 @@ const config = require('../config');
 const Entity = require('poplar').Entity
 const debug = require('debug')('openapi:adapter:poplar')
 
-function PoplarSwaggerAdapter (options) {
+const DEFAULT_RESPONSE_DEFINITION = {
+  name: 'response-format',
+  description: '默认输出数据格式',
+  type: 'object',
+  required: ['status', 'message'],
+  properties: {
+    'status': {
+      type: 'number',
+      enum: [0, 1, 2]
+    },
+    "message": {
+      type: 'string'
+    },
+    "metadata": {
+      type: 'object',
+      description: '分页信息',
+      properties: {
+        'maxid': {
+          type: 'string'          
+        },
+        'minid': {
+          type: 'string'
+        },
+        'count': {
+          type: 'number'
+        }
+      }
+    },
+    "errors": {
+      type: 'array',
+      items: {
+        type: 'string'
+      }
+    },
+    "data": {
+      type: 'object'
+    }
+  }
+}
+
+function PoplarSwaggerAdapter(options) {
   this.options = options
   this.APIRouter = options.APIRouter
+}
+
+PoplarSwaggerAdapter.prototype.init = function (parser) {
+  parser.createDefinition(DEFAULT_RESPONSE_DEFINITION)  
+}
+
+PoplarSwaggerAdapter.prototype.parseDefinitionTree = function (root) {
+  const definition = {
+    type: "object",
+    properties: {},
+    required: [],
+    description: root.description || ""
+  }
+
+  Object.keys(root)
+    .forEach(k => {
+      const item = root[k]
+      definition.properties[k] = {}
+      const itemDef = definition.properties[k]
+
+      if (Array.isArray(item)) {
+        itemDef.type = 'array'
+        itemDef.items = this.parseDefinitionTree(item[0])
+      } else if (!item.type) {
+        itemDef.type = 'object'
+        const subConfig = this.parseDefinitionTree(item)
+        itemDef.properties = subConfig.properties
+      } else {
+        Object.assign(itemDef, config.MONGOOSE_TYPE_MAPPING[item.type.toLowerCase()])
+        if (item.required) {
+          definition.required.push(k)
+        }
+        if (item.description) {
+          itemDef.description = item.description
+        }
+        if (item.default) {
+          itemDef.default = item.default
+        }
+        if (item.enum) {
+          itemDef.enum = item.enum
+        }
+      }
+    })
+
+  return definition
 }
 
 PoplarSwaggerAdapter.prototype.parseResponse = function (method, parser) {
   const responses = Object.assign({}, config.DEFAULT_RESPONSE)
   const entity = method.presenter || (method.notes && method.notes.entity)
   const schema = method.notes && method.notes.schema
-  // assert(Entity.isEntity(entity), 'presenter or notes.entity must be a valid Entity');
 
   if (!schema || !entity) {
     debug('using default response')
     return responses
   }
 
-  const excepts = entity._excepts.slice()
   const exposes = entity._mappings
-  const definition = {
-    type: 'object',
-    name: entity._name,
-    properties: {}
-  }
+  const excepts = entity._excepts.slice()
+  let rawDefinition = Object.assign({}, schema.obj, exposes)
+  const definition = this.parseDefinitionTree(rawDefinition)
+  definition.name = method.name + '-response'
 
-  Object.keys(schema)
-    .concat(Object.keys(exposes))
-    .forEach(k => {
-      const conf = exposes[k] || schema[k]
-      const type = conf.type.toLowerCase()
-      const def = Object.assign({}, config.MONGOOSE_TYPE_MAPPING[type])
-      if (conf.enum) {
-        def.enum = conf.enum
-      }
-      if (conf.default) {
-        def.default = conf.default
-      }
-      definition.properties = Object.assign(definition.properties, def)
-    })
-  
-  //remove alias key
   Object.keys(exposes).forEach(k => {
     if (exposes[k].act === 'alias') {
       definition.properties[exposes[k].name] = definition.properties[k]
-      delete definition.properties[k]      
+      delete definition.properties[k]
     }
   })
 
-  //remove excepts field
-  exports.forEach(k => {
-    delete definition.properties[k]    
-  })
+  definition.properties = Object.keys(definition.properties)
+    .filter(k => excepts.indexOf(k) === -1)
+    .reduce((acc, cur) => {
+      acc[cur] = definition.properties[cur]
+      return acc
+    }, {})
 
   const resp = {
     "200": {
@@ -63,25 +131,23 @@ PoplarSwaggerAdapter.prototype.parseResponse = function (method, parser) {
       }
     }
   }
-  //TODO parse nested array 、 object、entity using
+
+  //TODO entity using
   parser.createDefinition(definition)
 
   return Object.assign(responses, resp)
 }
 
 PoplarSwaggerAdapter.prototype.parsePathTemplateParams = function (method) {
-  debug('http: ', method.http)
-  debug('http.path: ', method.http.path)
   return method.http.path.split('/')
-  .filter(it => it && it[0] === ':')
-  .map(it => it.slice(1))
-  .map(it => {
-    return {
-      in: 'path',
-      name: it,
-      required: true
-    }
-  })
+    .filter(it => it && it[0] === ':')
+    .map(it => it.slice(1))
+    .map(it => {
+      return { in: 'path',
+        name: it,
+        required: true
+      }
+    })
 }
 
 PoplarSwaggerAdapter.prototype.parseQueryParams = function (tags, method, parser) {
@@ -89,10 +155,9 @@ PoplarSwaggerAdapter.prototype.parseQueryParams = function (tags, method, parser
 
   return method.accepts.map(paramter => {
     const pathParamter = pathParams.find(it => it.name === paramter.arg)
-    const defaultConf = {
-      in: 'query',
+    const defaultConf = { in: 'query',
       name: paramter.arg,
-      required: (paramter.required || (paramter.validates && paramter.validates.required) || false)   
+      required: (paramter.required || (paramter.validates && paramter.validates.required) || false)
     }
     const conf = pathParamter || defaultConf
     conf.description = paramter.description || ""
@@ -113,17 +178,16 @@ PoplarSwaggerAdapter.prototype.parseQueryParams = function (tags, method, parser
 }
 
 PoplarSwaggerAdapter.prototype.parseBodyParams = function (tags, method, parser) {
-  const parameter = {
-    in: 'body',
+  const parameter = { in: 'body',
     name: "data",
     description: method.name + ' params'
   }
   const definition = {
     type: 'object',
-    required: [],    
+    required: [],
     name: tags[0] + method.name,
   }
-  
+
   definition.properties = method.accepts.reduce((acc, paramter) => {
     const conf = {}
     if (Array.isArray(paramter.type)) {
@@ -142,7 +206,9 @@ PoplarSwaggerAdapter.prototype.parseBodyParams = function (tags, method, parser)
   }, {})
 
   parser.createDefinition(definition)
-  parameter.schema = { $ref: '#/definitions/' + definition.name }
+  parameter.schema = {
+    $ref: '#/definitions/' + definition.name
+  }
 
   return [parameter]
 }
@@ -159,7 +225,7 @@ PoplarSwaggerAdapter.prototype.parseOperation = function (method) {
 
 PoplarSwaggerAdapter.prototype.parseParamsIn = function (method) {
   const verb = method.http.verb
-  
+
   if (verb.toLowerCase() === 'get') {
     return 'query'
   } else {
@@ -184,26 +250,33 @@ PoplarSwaggerAdapter.prototype.parseMethod = function (tags, method, parser) {
   }
 
   const pathName = '/' + method._apiBuilder.name + (conf.operationId ? ('/' + conf.operationId) : '')
-  const path = parser.createPath({ name: pathName })
+  const path = parser.createPath({
+    name: pathName
+  })
 
-  switch(this.parseParamsIn(method)) {
-    case 'query': {
-      conf.parameters = this.parseQueryParams(tags, method, parser) 
-      break
-    }
-    case 'body': {
-      conf.parameters = this.parseBodyParams(tags, method, parser)      
-      break
-    }
-    case 'header': {
-      break
-    }
-    case 'form': {
-      break
-    }
-    default: {
-      break
-    }
+  switch (this.parseParamsIn(method)) {
+    case 'query':
+      {
+        conf.parameters = this.parseQueryParams(tags, method, parser)
+        break
+      }
+    case 'body':
+      {
+        conf.parameters = this.parseBodyParams(tags, method, parser)
+        break
+      }
+    case 'header':
+      {
+        break
+      }
+    case 'form':
+      {
+        break
+      }
+    default:
+      {
+        break
+      }
   }
 
   conf.responses = this.parseResponse(method, parser)
@@ -214,10 +287,14 @@ PoplarSwaggerAdapter.prototype.parseMethod = function (tags, method, parser) {
 PoplarSwaggerAdapter.prototype.parse = function (parser) {
   const APIRouter = this.APIRouter
 
+  this.init(parser)
   Object.keys(APIRouter._apiBuilders).forEach(tagName => {
     debug('apiBuilder name: ', tagName)
     const apiBuilder = APIRouter._apiBuilders[tagName]
-    const tag = parser.createTag({ name: tagName, description: `The ${tagName} tag`})
+    const tag = parser.createTag({
+      name: tagName,
+      description: `The ${tagName} tag`
+    })
 
     Object.keys(apiBuilder._methods).forEach(name => {
       const method = apiBuilder._methods[name]
